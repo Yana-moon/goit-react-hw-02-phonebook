142b1025a35741ee71eab5fba8f1671df14ad02e
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-04-05 10:29:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ export class App extends Component {
     filter: '',
   };
 
-  AddContact = ( name, number ) => {
+  addContact = ( name, number ) => {
     const contact = {
       id: nanoid(),
       name,
